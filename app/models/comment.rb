8c0b67900d10e8c4af6d5fc98baad9666bbca3ec
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-12 21:43:39 +0000

    fixed venue comments form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,10 +14,10 @@ class Comment < ApplicationRecord
   validates(:commenter, { :presence => true })
 
   def commenter
-    my_id = self.id
+    author_id = self.author_id
+    matching_author = User.where({ :id => author_id})
+    the_user = matching_author.first
     
-    matching_users = User.where({ :id => my_id })
-    the_user = matching_users.at(0)
     return the_user
   end
 end
